8481506e6bb6d9c2e25149f1fccf4af78e54d0cb
Danish M <mohd.danish0419@gmail.com>
2024-08-25 17:46:22 -0400

    Added Headless mode execution for Chrome browser

--- src/main/java/com/testing/medicure_test_app/App.java
+++ src/main/java/com/testing/medicure_test_app/App.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Hello world!
@@ -15,10 +16,10 @@ public class App
     public static void main( String[] args ) throws InterruptedException
     {
     	//initialize the chrome driver
-    			//ChromeOptions options = new ChromeOptions();
-    	    	//options.addArguments("--headless");
+    			ChromeOptions options = new ChromeOptions();
+    	    	options.addArguments("--headless");
     	    	
-    		 	WebDriver driver = new ChromeDriver(/*options*/);
+    		 	WebDriver driver = new ChromeDriver(options);
     		 	
     		 	//open the web application
     	        driver.get("http://44.200.151.7:8082/contact.html");
@@ -35,28 +36,28 @@ public class App
     	        name.sendKeys("Danish");
     	            	            	        
     	        System.out.println(name);
-    	        Thread.sleep(3000);     
+    	        Thread.sleep(500);     
     	        
     	        //2. Locate and Enter Phone
     	        WebElement number = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[1]/div[2]/div/input"));
     	        number.sendKeys("999999999");
     	        
     	        System.out.println(number);
-    	        Thread.sleep(3000);
+    	        Thread.sleep(500);
     	        
     	        //3. Locate Email and Enter Email
     	        WebElement mail = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[2]/input"));
     	        mail.sendKeys("danish@xyz.com");
     	        
     	        System.out.println(mail);
-    	        Thread.sleep(3000);
+    	        Thread.sleep(500);
     	        
     	        //4. Locate Message and Enter Message
     	        WebElement message = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[3]/input"));
     	        message.sendKeys("Hello, How are you ?");
     	        
     	        System.out.println(message);
-    	        Thread.sleep(3000);
+    	        Thread.sleep(500);
     	        
     	       //5. Locate Send Button and click to send the message.
     	        WebElement sendButton = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[4]/button"));
